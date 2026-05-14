1aa447c64afed47b0c7891259ee033673037128a
young-feed <danilovdanyaivanovi4@gmail.com>
2026-05-14 14:19:56 +0300

    http

--- hw.py
+++ hw.py
@@ -1,5 +1,12 @@
-h = "Hello"
-_ = " "
-w = "World"
+from flask import Flask
 
-print(h+_+w)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # host='0.0.0.0' позволяет принимать запросы со всех интерфейсов
+    # port=5000 — стандартный порт для Flask
+    app.run(host='0.0.0.0', port=5000)
